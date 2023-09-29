6483c1a262cecd192586f7a938237fd3b1810c6d
V1onuess <129693346+v1onues@users.noreply.github.com>
2023-09-30 00:39:21 +0300

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -9,10 +9,10 @@ export default function Footer(){
         <div align="center">
             <div className="divider"/>
             <div className="flex flex-col sm:flex-row max-w-8xl justify-between items-center py-8 w-11/12 sm:w-10/12 mx-auto">
-                <h1 className="font-sans text-white text-2xl font-semibold flex gap-2 items-end">Just Nyde
-                <div className="bg-primary-100 text-sm px-2 py-2 font-sans text-gray-400 rounded-lg">v2.0</div></h1>
+                <h1 className="font-sans text-white text-2xl font-semibold flex gap-2 items-end">V1onuess.
+                <div className="bg-primary-100 text-sm px-2 py-2 font-sans text-gray-400 rounded-lg">v1.0</div></h1>
                 <h1 className="font-sans text-white text-base flex items-center gap-3">{(user == null) ? (<div className="w-3 h-3 rounded-full bg-red-600"/>) : (<div className="w-3 h-3 rounded-full bg-green-600"/>)} Data fetching</h1>
-                <h1 className="font-sans text-white text-base flex items-center gap-1">Created with <AiOutlineHeart className="text-rose-600"/> by <span className="text-primary">Just Nyde</span></h1>
+                <h1 className="font-sans text-white text-base flex items-center gap-1">Created with <AiOutlineHeart className="text-rose-600"/> by <span className="text-primary">V1onuess.</span></h1>
             </div>
         </div>
     )
