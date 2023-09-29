f5e935decb1c4a3e2cdd6e2b94d7333cb3436b31
V1onuess <129693346+v1onues@users.noreply.github.com>
2023-09-30 00:20:17 +0300

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar(){
     return (<>
         <div className="flex justify-between items-center max-w-none sm:max-w-8xl mx-auto w-full sm:w-11/12 p-9">
             <div>
-                <h1 className="font-sans text-white font-semibold text-2xl">nyde<span className="text-primary">.live</span></h1>
+                <h1 className="font-sans text-white font-semibold text-2xl">vion<span className="text-primary">.pw</span></h1>
             </div>
             <div className="flex items-center space-x-5">
                 <NavElements/>
